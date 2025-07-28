9804078edd2744350b5a1f81fbc5d61340b19b04
Mahnoor Hanif <ROONHAM041@GMAIL.COM>
2025-07-28 07:49:34 +0500

    Update RemoveElments.cpp

--- Array/RemoveElments.cpp
+++ Array/RemoveElments.cpp
@@ -1,1 +1,20 @@
+/ LeetCode #27 - Remove Element
+// Remove all occurrences of val from nums in-place and return new length.
 
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;  // index for placing elements not equal to val
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];  // keep this element
+            }
+        }
+        
+        return k;  // new length without val
+    }
+}
